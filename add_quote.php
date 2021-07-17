138c31f76261b547aa4652a4e7b4cfe3bc8df6cb
Joeri <joeri@elephantcs.nl>
2021-07-17 22:59:50 +0200

    Loki“Oh dear.Is she dead?”

--- add_quote.php
+++ add_quote.php
@@ -1,13 +1,21 @@
 <?php
+$path = getcwd();
+
 
 $quotes   = json_decode(file_get_contents('quotes.json'), true);
 $quotes[] = [
     'name'  => $argv[1],
     'quote' => $argv[2],
 ];
-var_dump($quotes[array_key_last($quotes)]);
+
+$last_quote = $quotes[array_key_last($quotes)];
+
+var_dump($last_quote);
 
 file_put_contents('quotes.json', json_encode($quotes));
+
+exec("git add .");
+exec("git commit -m'" . $last_quote['name'] . $last_quote['quote'] ."'");
 
 function removeFirstAndLastChar(string $string)
 {
